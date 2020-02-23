95bc1119f52de6d979eea90683a48152f0d52211
Tomasz Bernat <tomasz.bernat@stava.pl>
2020-02-23 22:16:49 +0100

    Adds Item model to set of project models.

--- todolist_engine/models.py
+++ todolist_engine/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    class Meta:
+        verbose_name='Todo item'
+        verbose_prular_name='Todo items'
+        ordering=['-add_timestamp']
+
+    description = models.TextField()
+    add_timestamp = models.DateTimeField(auto_now=True)
+    is_done = models.BooleanField(default=False)
