68fc6e336d9c1706cf428fc3114826928754569f
tbernat <tomasz.bernat@stava.pl>
2020-02-28 18:31:23 +0100

    #1 - Creates item model as a representation for todo item.

--- todolist_engine/models.py
+++ todolist_engine/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+
+    class Meta:
+        verbose_name = 'Todo item'
+        verbose_name_plural = 'Todo items'
+        ordering = ['-add_timestamp', 'is_done']
+
+    todo_description = models.TextField(null=False, blank=False)
+    is_done = models.BooleanField(default=False)
+    add_timestamp = models.DateTimeField(auto_now=True)
+    done_timestamp = models.DateTimeField(null=False, blank=False)
