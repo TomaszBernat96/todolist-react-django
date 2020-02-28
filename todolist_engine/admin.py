d382db105884e1803aaa5d0fe916dde33991851f
tbernat <tomasz.bernat@stava.pl>
2020-02-28 19:14:53 +0100

    #1 - Creates adds necessary models fields and creates admin page for model.

--- todolist_engine/admin.py
+++ todolist_engine/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from todolist_engine.models import Item
 
 # Register your models here.
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('is_done', 'add_timestamp', 'todo_title', 'done_timestamp')
--- todolist_engine/models.py
+++ todolist_engine/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,7 +11,10 @@ class Item(models.Model):
         verbose_name_plural = 'Todo items'
         ordering = ['-add_timestamp', 'is_done']
 
+    user = models.ForeignKey('User', on_delete=models.CASCADE, null=False, blank=False)
+    todo_title = models.TextField(null=False, blank=False)
     todo_description = models.TextField(null=False, blank=False)
-    is_done = models.BooleanField(default=False)
+    slug = models.CharField(max_length=256)
     add_timestamp = models.DateTimeField(auto_now=True)
+    is_done = models.BooleanField(default=False)
     done_timestamp = models.DateTimeField(null=False, blank=False)
