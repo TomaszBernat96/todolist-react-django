d9ce88c7def422bb265c53c652467ee25fef0ccd
Tomasz Bernat <tomasz.bernat@stava.pl>
2020-03-02 20:55:17 +0100

    Adjusts admin page for todo item

--- todolist_engine/admin.py
+++ todolist_engine/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Item
 # Register your models here.
 
+admin.AdminSite.empty_value_display = '-'
+
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    fields = ()
+    fields = ('user', 'title', 'add_timestamp', 'is_done', 'done_timestamp')
