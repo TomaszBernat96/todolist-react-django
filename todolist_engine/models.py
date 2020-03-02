29f5eeb90ecbfb0cb295b85dff4fb09f372c5808
Tomasz Bernat <tomasz.bernat@stava.pl>
2020-03-02 20:26:54 +0100

    Adds model import into admin and extends todo item model.

--- todolist_engine/models.py
+++ todolist_engine/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256, unique=True)
+    slug = models.SlugField(max_length=64)
+    description = models.TextField(max_length=2048)
+    add_timestamp = models.DateTimeField(auto_now=True)
+    done_timestamp = models.DateTimeField(blank=True)
+    is_done = models.BooleanField(default=False)
+
     class Meta:
         verbose_name='Todo item'
         verbose_name_plural='Todo items'
-        ordering=['-add_timestamp']
-
-    description = models.TextField()
-    add_timestamp = models.DateTimeField(auto_now=True)
-    is_done = models.BooleanField(default=False)
+        ordering=['-add_timestamp', '-is_done']
+        constraints = [
+            models.UniqueConstraint(fields=['title', 'slug'], name='unique_title'),
+            models.CheckConstraint(check=models.Q(done_timestamp__lt=models.F('add_timestamp')), name='done_ts_verification'),
+        ]
